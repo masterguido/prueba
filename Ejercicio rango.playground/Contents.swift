cde676b649f8d1012f38d84241ddecb30e042b0f
masterguido <master@unknown-3c:07:54:5c:b4:2c.home>
2015-11-10 12:27:30 -0300

    Tarea semana 2
    Ejercicio de rangos

---
+++ Ejercicio rango.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+print ("Ejercicio de Carlos Guidobono\n")
+print("N.Resultado \n")
+print("================\n")
+for i in 0...100{
+    if i % 2 == 0{
+            print ("\(i) Par ")
+    }else{
+            print("\(i) Impar ")
+    }
+    if i % 5 == 0{
+            print(" Bingo!!!")
+    }
+    if i >= 30 && i <= 40{
+            print (" Viva Swift!!! ")
+      
+    }
+        
+    
+    print("\n")
+}
